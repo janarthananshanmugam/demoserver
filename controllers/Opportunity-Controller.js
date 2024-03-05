d4a5e66ac5e728fd3a1ee16a72d597ab61e39a11
Janarthanan <janarthanan.shanmugam@kovaion.com>
2024-03-05 20:56:01 +0530

    demo

--- controllers/Opportunity-Controller.js
+++ controllers/Opportunity-Controller.js
@@ -1,28 +1,28 @@
 const leadData = [
-    {
-        id:1,
-        name : "Jana1"
-    },
-    {
-        id:2,
-        name : "Jana2"
-    },
-    {
-        id:3,
-        name : "Jana3"
-    }
-]
+  {
+    id: 1,
+    name: "Jana1",
+  },
+  {
+    id: 2,
+    name: "Jana2",
+  },
+  {
+    id: 3,
+    name: "Jana3",
+  },
+];
 
-const getOpportunity = (req,res) =>{
-    res.json(leadData)
-}
+const getOpportunity = (req, res) => {
+  console.log(leadData);
+  res.json(leadData);
+};
 
-const getOpportunityById = (req,res) =>{
-    res.send(leadData)
-}
+const getOpportunityById = (req, res) => {
+  res.send(leadData);
+};
 
-
-module.exports={
-    getOpportunity,
-    getOpportunityById
-}
+module.exports = {
+  getOpportunity,
+  getOpportunityById,
+};
